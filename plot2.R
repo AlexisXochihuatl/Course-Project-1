e6e4132ba006677e01783f4ee6d31ce4a2c34613
AlexisXochihuatl <48746302+AlexisXochihuatl@users.noreply.github.com>
2019-04-07 17:53:26 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,1 +1,13 @@
-
+#Reads in data from file then subsets data for specified dates
+D.T <- data.table::fread(input = "household_power_consumption.txt",na.strings="?")
+# Prevents Scientific Notation
+D.T[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+# Making a POSIXct date capable of being filtered and graphed by time of day
+D.T[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+# Filter Dates for 2007-02-01 and 2007-02-02
+D.T <- D.T[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("plot2.png", width=480, height=480)
+## Plot 2
+plot(x = D.T[, dateTime],y = D.T[, Global_active_power],type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
+plot(x = D.T[, dateTime],y = D.T[, Global_active_power],type="l", xlab="", ylab="Global Active Power (kilowatts)")
